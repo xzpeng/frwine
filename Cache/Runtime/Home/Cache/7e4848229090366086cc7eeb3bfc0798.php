3a540d62a064d359e66bb8a55b9b02129c2b49a2
splio-xzpeng <xzpeng@splio.cn>
2014-01-02 16:34:52 +0800

    -Init

---
+++ Cache/Runtime/Home/Cache/7e4848229090366086cc7eeb3bfc0798.php
@@ -0,0 +1,15 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!--    <script type="text/javascript" src="/Public/Min/?f=/Public/Js/jquery-1.9.0.min.js|/Public/Js/jquery.lazyload.js"></script>
+            <link rel="stylesheet" type="text/css" href="/Public/Min/?f=/Public/Css/base.css|/Public/Css/layout.css" />-->
+    </head>
+    <body>
+
+        <script type="text/javascript">
+        </script>
+        index
+    </body>
+</html>
