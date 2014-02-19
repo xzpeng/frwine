8b286b6976ec3bb80fb8c5b3500131a56eb2ef5c
xzpeng <pengxiaozhou2008@gmail.com>
2014-02-19 20:35:24 +0800

    2014-02-19 20:35:11

---
+++ Cache/Runtime/Home/Cache/58b0c8ebb40e8ad805b64f5bd9e08759.php
@@ -0,0 +1,17 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script type="text/javascript" src="/Public/Min/?f=/Public/Js/jquery-1.9.0.min.js|/Public/Js/jquery.lazyload.js|/Public/Js/dic/jquery.autocomplete.min.js|/Public/Js/dic/dictionary.js"></script>
+        <link rel="stylesheet" type="text/css" href="/Public/Min/?f=/Public/Css/base.css|/Public/Css/layout.css|/Public/Css/dic/jquery.autocomplete.css" />
+    </head>
+    <body>
+        <div>
+            <form action method="POST">
+                <input type="text" id="dict" />
+                <input type="submit" value="SEARCH" />
+            </form>
+        </div>
+    </body>
+</html>
--- Home/Lib/Action/DictionaryAction.class.php
+++ Home/Lib/Action/DictionaryAction.class.php
@@ -7,4 +7,17 @@ class DictionaryAction extends Action {
         $this->display();
     }
 
+    public function search() {
+
+        $keyword = $_GET['keyword'];
+        $M = M("Dictionary");
+        $map = array();
+
+        $map['frname'] = array('like', '%' . $keyword . '%');
+        //$list = $M->field("`id`,`frname`,`chname`,`description`,`region`,`update_time`")->order("`id` DESC")->limit("$firstRow , $listRows")->select();
+        $list = $M->where($map)->select();
+//echo $M->getLastSql();
+        var_dump($list);
+    }
+
 }
